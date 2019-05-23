5e7691be99114e4db14fe4bfdb97c950dced9280
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2019-05-23 09:34:36 +0800

    Add files via upload

---
+++ Mod2_Lab4/Mod2_Lab4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = 1;
+
+            //while(n<6)
+            //{
+            //    Console.WriteLine($"Current value of n is {n}");
+            //    n++;
+            //}
+
+            int x = 0;
+
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x < 5);
+        }
+    }
+}
